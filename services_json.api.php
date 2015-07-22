02af8d45378d9a0ed61dc301e1ebd28fdc742ab8
Vijayachandran Mani <vijaycs85@gmail.com>
2015-07-22 12:13:23 +0100

    Initial implementation

---
+++ services_json.api.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Services JSON module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Provides endpoints definitions.
+ *
+ * @return array
+ *   An array of endpoint definition.
+ */
+function hook_services_json_info() {
+  return array(
+
+  );
+}
